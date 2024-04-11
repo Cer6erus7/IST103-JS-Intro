8600a6fdbbf81d66fe00497b8e1570e4517845bb
Roker <matvey14481@gmail.com>
2024-04-10 19:54:01 -0700

    added logic to the variables

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //alert("Hello World")
 
+const Gender = "Male"
+
 let PersonName = "Wilson"
 let PersonAge = 30
 let IsMarried = false
@@ -7,5 +9,11 @@ let IsEmployed = true
 let AmountOfKids = null
 let Salary =
 
-const Gender = "Male"
+console.log("Person's name is " + PersonName)
+console.log(PersonName + "'s age is " + PersonAge)
+console.log("Is " + PersonName + " married - " + IsMarried)
+console.log("Is " + PersonName + " employed - " + IsEmployed)
+console.log(PersonName + "'s amount of kids - " + AmountOfKids)
+console.log(PersonName + "'s salary is - " + Salary)
+console.log(PersonName + "'s gender is - " + Gender)
 
