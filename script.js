1a57f064acfe21468b9e355c13a660b4671adc08
Roker <matvey14481@gmail.com>
2024-04-10 21:13:19 -0700

    Made some logic for Object

--- script.js
+++ script.js
@@ -17,4 +17,17 @@
 //console.log(PersonName + "'s salary is - " + Salary)
 //console.log(PersonName + "'s gender is - " + Gender)
 
-let ToDoList = ["Make this assignment", "Go to the Vancouver", "Learn a new language JavaScript", "Learn more about HTML and CSS"]
+//let ToDoList = ["Make this assignment", "Go to the Vancouver", "Learn a new language JavaScript", "Learn more about HTML and CSS"]
+
+let MyPhone = {
+    mark: "Iphone",
+    model: "12 Max",
+    year: 3,
+    color: "black",
+    amount_of_cameras: 3
+}
+
+console.log("My phone is "  + MyPhone.mark + MyPhone.model)
+console.log("I used this phone for " + MyPhone.year + " year")
+console.log("Phone's color is " + MyPhone.color)
+console.log("Amount of cameras are " + MyPhone.amount_of_cameras)
