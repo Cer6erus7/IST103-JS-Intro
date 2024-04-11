d22f6d106d50ecdda23dab8b9a0937796a9c88e0
Roker <matvey14481@gmail.com>
2024-04-10 19:37:43 -0700

    made some variables in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello World")
+//alert("Hello World")
+
+let PersonName = "Wilson"
+let PersonAge = 30
+let IsMarried = false
+let IsEmployed = true
+let AmountOfKids = null
+let Salary =
+
+const Gender = "Male"
+
