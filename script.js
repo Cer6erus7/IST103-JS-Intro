7b6236b598049d40b1f6bde3e65e558ca84423c4
Roker <matvey14481@gmail.com>
2024-04-10 20:58:08 -0700

    made a new list

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 //alert("Hello World")
 
-const Gender = "Male"
+//const Gender = "Male"
+//
+//let PersonName = "Wilson"
+//let PersonAge = 30
+//let IsMarried = false
+//let IsEmployed = true
+//let AmountOfKids = null
+//let Salary =
+//
+//console.log("Person's name is " + PersonName)
+//console.log(PersonName + "'s age is " + PersonAge)
+//console.log("Is " + PersonName + " married - " + IsMarried)
+//console.log("Is " + PersonName + " employed - " + IsEmployed)
+//console.log(PersonName + "'s amount of kids - " + AmountOfKids)
+//console.log(PersonName + "'s salary is - " + Salary)
+//console.log(PersonName + "'s gender is - " + Gender)
 
-let PersonName = "Wilson"
-let PersonAge = 30
-let IsMarried = false
-let IsEmployed = true
-let AmountOfKids = null
-let Salary =
-
-console.log("Person's name is " + PersonName)
-console.log(PersonName + "'s age is " + PersonAge)
-console.log("Is " + PersonName + " married - " + IsMarried)
-console.log("Is " + PersonName + " employed - " + IsEmployed)
-console.log(PersonName + "'s amount of kids - " + AmountOfKids)
-console.log(PersonName + "'s salary is - " + Salary)
-console.log(PersonName + "'s gender is - " + Gender)
-
+let ToDoList = ["Make this assignment", "Go to the Vancouver", "Learn a new language JavaScript", "Learn more about HTML and CSS"]
